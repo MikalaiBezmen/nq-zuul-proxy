dec7e3181ac0b7e1829b1b3b8d2ebd2826d0c588
Mikalai_Bezmen <Mikalai_Bezmen@epam.com>
2017-10-04 10:04:23 +0300

    init

---
+++ src/main/java/net/javacasts/cloud/ProxyApplication.java
@@ -0,0 +1,21 @@
+package net.javacasts.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
+
+    @Bean
+    public PatternServiceRouteMapper serviceRouteMapper() {
+        return new PatternServiceRouteMapper("(?<name>^.+)-(?<version>v.+$)", "${version}/${name}");
+    }
+}
